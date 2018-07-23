5a7bf2b0792f160cf5eb2fee529ee9ceeb5cc779
Scott <scott@MacBook-Pro.local>
2018-07-23 13:38:33 -0600

    initial upload

---
+++ src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hero">
+    <div>
+      <br>
+      <br>
+      <br><br>
+      <img src="../assets/bikeimage.jpeg" alt="Zootown Bike Share" height="400" width="700">
+      <br><br>
+      <hr>
+      <h1 class="display-3">Hello Zootown Bike Share World</h1>
+      <p class="lead">Making Bikes Feel Loved, Needed and Wanted....Everyday!</p>
+      <br>
+      <br>
+    </div>
+  </div>
+</template>
+
+<style>
+  .hero {
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+  .hero .lead {
+    font-weight: 200;
+    font-size: 1.5rem;
+  }
+</style>
